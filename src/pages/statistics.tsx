dd3219fa86f585e43265a5ef79dfc5f4268f4795
jonvabalis <tmotiejus@gmail.com>
2025-04-24 20:49:39 +0300

    feat: added tabs in statistics page

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -5,6 +5,10 @@ import {
   useGetByNameBerryType,
 } from "../api/berryTypes/useGetByNameBerryType";
 import StatisticsBox from "../components/Statistics/StatisticsBox";
+import { Tabs, Tab, Grid2 } from "@mui/material";
+import { useState } from "react";
+import { BoxPaper } from "../components/Reusable/BoxPaper";
+import TabPanel from "../components/Reusable/TabPanel";
 
 export default function statistics() {
   const savedBerryType = localStorage.getItem("berryType");
@@ -13,6 +17,12 @@ export default function statistics() {
     : "not found";
   const { data: berryTypeData, isLoading } =
     useGetByNameBerryType(berryTypeName);
+
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleTabChange = (_e: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
   if (isLoading) {
     return <PageHeader text="Data is being fetched" />;
@@ -27,7 +37,38 @@ export default function statistics() {
       <Box>
         <PageHeader text="Statistics" />
       </Box>
-      <StatisticsBox berryTypeData={berryTypeData} />
+      <BoxPaper>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Tabs
+            value={tabValue}
+            onChange={handleTabChange}
+            sx={{
+              [`& .MuiTabs-flexContainer`]: {
+                flexWrap: "wrap",
+                justifyContent: "center",
+              },
+              "& .MuiTabs-indicator": {
+                display: "none",
+              },
+            }}
+          >
+            <Tab label="Berry Statistics" id="statistics-tab-0" />
+            <Tab label="Employee Statistics" id="statistics-tab-1" />
+            <Tab label="Global Statistics" id="statistics-tab-2" />
+            <Tab label="Global Employee Statistics" id="statistics-tab-3" />
+          </Tabs>
+        </Box>
+      </BoxPaper>
+      <Box sx={{ mt: 4 }} />
+
+      <TabPanel value={tabValue} index={0}>
+        <StatisticsBox berryTypeData={berryTypeData} />
+      </TabPanel>
     </Box>
   );
 }
